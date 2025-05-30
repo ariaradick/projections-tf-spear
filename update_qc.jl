db9212ac09f66bce24e789b496110a2844dc920e
Aria Radick <aradick@uoregon.edu>
2025-05-30 16:00:55 -0700

    added vector input support

--- update_qc.jl
+++ update_qc.jl
@@ -2,25 +2,37 @@ using DataFrames
 using CSV
 
 const catalog_csv = "spear_catalog_blue.csv"
-const allowed_column_names = ["variable_id", "platform", "time_range"]
+const allowed_keys = ["variable_id", "experiment", "time_range"]
+
+function string_to_vector(x)
+    strip.(split(strip(x, ['[',']',' ']),','))
+end
 
 function body_to_dict(input_string)
     y = split.(strip.(split(input_string,'\n')),':')
     y = y[length.(y) .== 2]
     y = permutedims(hcat(y...))
     y = strip.(y)
-    truths = [(x in allowed_column_names) for x in y[:,1]]
+    truths = [(x in allowed_keys) for x in y[:,1]]
     y = y[truths,:]
-    return Dict([(String(y[i,1]),String(y[i,2])) for i in 1:size(y)[1]])
+    return Dict([(String(y[i,1]),String.(string_to_vector(y[i,2]))) for i in 1:size(y)[1]])
 end
 
 function find_rows(df, columns)
     truths = ones(Bool, size(df)[1])
     for (k,v) in columns
-        if k == "platform"
-            truths = (truths .& occursin.(v,df[!,k]))
+        if k == "experiment"
+            subtruths = zeros(Bool, size(df)[1])
+            for x in v
+                subtruths = (subtruths .| occursin.(x,df[!,:platform]))
+            end
+            truths = (truths .& subtruths)
         else
-            truths = (truths .& (df[!,k] .== v))
+            subtruths = zeros(Bool, size(df)[1])
+            for x in v
+                subtruths = (subtruths .| (df[!,k] .== x))
+            end
+            truths = (truths .& subtruths)
         end
     end
     return truths
